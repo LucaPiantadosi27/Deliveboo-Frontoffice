591864030814888d37b2c3cc49561b88494c612f
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-05-16 17:54:13 +0200

    aggiunta chiamata api categorie

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -7,15 +7,50 @@ export default {
     components: {
 
     },
+
+    data() {
+        return {
+            baseApiUrl: "http://127.0.0.1:8000/api/",
+            categories: [],
+        }
+    },
+
+    methods: {
+        // initial api call to retrieve restaurant categories
+        apiCall(){
+            axios.get(this.baseApiUrl + "categories").then((res) => {
+                this.categories = res.data.results;
+                console.log(this.categories);
+            })
+        }
+    },
+
+    mounted() {
+        this.apiCall();
+    },
 }
 </script>
 
 <template>
 <section>
-    <h1>TEST</h1>
+    <div class="container py-5">
+        <h1 class="text-center">DeliveBoo</h1>
+        <ul class="d-flex gap-2 justify-content-center">
+            <li v-for="category in categories" class="fs-4 ">
+                <a href="#!" class="text-decoration-none text-uppercase">{{ category.name }}</a>
+            </li>
+        </ul>
+    </div>
 
 </section>
 
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+
+ul{
+    list-style: none;
+}
+
+
+</style>
