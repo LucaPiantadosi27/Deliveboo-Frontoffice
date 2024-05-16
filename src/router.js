8f24f44ed058bd508f380f7e49f3b3766e0de5be
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-05-16 16:43:24 +0200

    impostazione router

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRoute({
+    history: createWebHistory(),
+    routes: [
+        
+    ]
+})
+
+export {router};
