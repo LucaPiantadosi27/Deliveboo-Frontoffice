c5ca415af94b7210c5470ab4435a0c2d39ca54ad
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-17 21:47:48 +0200

    fix style and footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -33,13 +33,13 @@ export default {
 <style lang="scss">
 
 footer {
-    
-        bottom: 0;
-        width: 100%;
-        height: 100px;
-        background-color: grey; 
-        z-index: 100; 
-        border-top: 1px solid white;
-    }
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 100px;
+    background-color: grey; 
+    z-index: 100; 
+    border-top: 1px solid white;
+}
 
 </style>
--- src/components/AppRestaurant.vue
+++ src/components/AppRestaurant.vue
@@ -25,27 +25,36 @@ export default {
       <h2 class="text-center">Ristoranti</h2>
       <div v-if="restaurants.length === 0" class="text-center">Nessun ristorante</div>
       <div v-for="restaurant in restaurants" :key="restaurant.id" class="restaurant-card">
-        <img :src="apiImageUrl + restaurant.img_res" class="card-img-top w-50" :alt="restaurant.name_res">
-        <h3>{{ restaurant.name_res }}</h3>
-        <h4>{{ restaurant.address_res }}</h4>
-        <div class="categories">
+        <img :src="apiImageUrl + restaurant.img_res" class="card-img-top mx-auto d-block w-50" :alt="restaurant.name_res">
+        <h3 class="text-center">{{ restaurant.name_res }}</h3>
+        <h4 class="text-center">{{ restaurant.address_res }}</h4>
+        <div class="categories text-center">
           <span v-for="category in restaurant.categories" :key="category.id" class="badge bg-secondary">{{ category.name }}</span>
         </div>
       </div>
     </div>
   </template>
-
-
-<style scoped>
-
-    .restaurant-card {
-        border: 1px solid #ccc;
-        padding: 15px;
-        margin-bottom: 10px;
-        border-radius: 5px;
-    }
-
-    .categories .badge {
-        margin-right: 5px;
-    }
-</style>
+  
+  <style scoped>
+  .restaurant-card {
+    padding: 15px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+  }
+  
+  .categories .badge {
+    margin-right: 5px;
+  }
+  
+  /* Rimuovi il contorno */
+  .restaurant-card img {
+    border: none;
+  }
+  
+  /* Centra l'immagine */
+  .restaurant-card img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  </style>
