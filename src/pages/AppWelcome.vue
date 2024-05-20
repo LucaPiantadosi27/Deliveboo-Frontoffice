f64eb65e4739a821cdabfe6d087f2001770dd384
mattia_morriale <mattia.morriale1@gmail.com>
2024-05-20 10:53:25 +0200

    create AppWelcome and router js

---
+++ src/pages/AppWelcome.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+
+export default {
+    name: "AppWelcome",
+
+    components: {
+        
+    },
+
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
+import AppWelcome from './pages/AppWelcome.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
+            name: 'welcome',
+            component: AppWelcome
+        },
+        {
+            path: '/home',
             name: 'home',
             component: AppHome
         },
