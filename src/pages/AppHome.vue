9adc16f166b59e2bdea48389b5c9d9d3754045d4
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-20 12:38:52 +0200

    style card-category

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -59,14 +59,13 @@ export default {
 <template>
   <section>
     <div class="container py-5">
-      <h1 class="text-center">DeliveBoo</h1>
       <div class="d-flex gap-2 justify-content-center">
         <div
         @click="AddCategory(category.id)"
         v-for="category in categories"
         :key="category.id"
         class="fs-4 d-flex gap-2 align-items-center">
-          <div class="card h-100 rounded-4 border-0" style="width: 150px;">
+          <div class="card-category card h-100 rounded-4 border-0" style="width: 150px;">
             <img class="category-img rounded-4" :src="'http://localhost:8000/storage/' + category.image" alt="">
             <div>{{ category.name }}</div>
           </div>
@@ -79,16 +78,26 @@ export default {
 
 
 <style lang="scss" scoped>
+    @use '../styles/variables' as *;
 
-    ul{
-        list-style: none;
+    section{
+
+      .card-category{
+
+        background-color: rgba(0, 0, 0, 0.4);
+        text-align: center;
+        cursor: pointer;
+        user-select: none;
+
+        .category-img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        }
+      }
+      
     }
 
-    .category-img{
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      cursor: pointer;
-    }
+    
 
 </style>
